f1936f6a9609d48fe4deddaafe9e6719cbbb15b0
SeokMin <dltjrals2@naver.com>
2021-06-21 09:56:08 +0900

    Python 문제 풀이 코드 :  2021-06-18 Binary Search 알고리즘 문제 풀이 수정
    1. 2021-06-18 Binary Search 알고리즘 문제 풀이 수정 (떡복이 떡 만들기)
    
    해결 :
    관련 :
    참고 :

--- Python_Algorithm/2021-06-18-binary-search2.py
+++ Python_Algorithm/2021-06-18-binary-search2.py
@@ -1,6 +1,5 @@
 n, m = map(int, input().split())
 lenArr = list(map(int, input().split()))
-lenArr.sort()
 
 # 10 15 17 19
 def binary_search(array, target, start, end):
@@ -8,18 +7,17 @@ def binary_search(array, target, start, end):
         count = 0
         mid = (start + end) // 2
 
-        for i in range(start, end + 1):
-            if mid < array[i]:
-                count += (array[i] - mid)
-            else:
-                continue
+        for x in lenArr:
+            if mid < x:
+                count += (x - mid)
 
         if target == count:
             return mid
+        # 떡의 양이 부족한 경우 : 더 많이 잘라야 한다. 왼쪽으로!
         elif target > count:
-            start = mid + 1
-        else:
             end = mid - 1
+        else:
+            start = mid + 1
     return None
 
-print(binary_search(lenArr, m, 0, lenArr[-1]))
+print(binary_search(lenArr, m, 0, max(lenArr)))

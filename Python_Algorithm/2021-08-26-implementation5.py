69a27a0f5ed1b00058ef4cb6a3cfcf729c331054
SeokMin <dltjrals2@naver.com>
2021-09-27 09:41:16 +0900

    Python 문제 풀이 코드 : 2021-08-26 Implementation 문제 풀이 수정
    1. 2021-08-26 Implementation 문제 풀이 수정
    (치킨 배달)
    
    해결 :
    관련 :
    참고 :

--- Python_Algorithm/2021-08-26-implementation5.py
+++ Python_Algorithm/2021-08-26-implementation5.py
@@ -1,42 +1,28 @@
-n, m = map(int, input().split())
-chicken_map = [[0] * n for _ in range(n)]
+from itertools import combinations
 
+n, m = map(int, input().split())
+house, chicken = [], []
 
 for i in range(n):
-    chicken_map[i] = list(map(int, input().split()))
+    map_info = list(map(int, input().split()))
+    for j in range(n):
+        if map_info[j] == 1:
+            house.append((i, j))
+        elif map_info[j] == 2:
+            chicken.append((i, j))
 
-def solution(n, m, chicken_map):
-    house_info = list()
-    chicken_info = list()
-    num = 0
+candidates = combinations(chicken, m)
 
-    for i in range(n):
-        for j in range(n):
-            # 집인 경우
-            if chicken_map[i][j] == 1:
-                house_info.append([i + 1, j + 1])
-            # 치킨 집인 경우
-            elif chicken_map[i][j] == 2:
-                chicken_info.append([i + 1, j + 1])
-                num += 1
-    # 폐업하는 가게의 수
-    close_door = num - m
-    print(close_door)
+def get_distance(candidate):
+    distance = 0
+    for hx, hy in house:
+        temp = 1e9
+        for cx, cy in candidate:
+            temp = min(temp, abs(hx - cx) + abs(hy - cy))
+        distance += temp
+    return distance
 
-    searchMinDistance(house_info, chicken_info, close_door)
-
-def searchMinDistance(house_info, chicken_info, close_door):
-    min_distance = 0
-    sum_list = list()
-
-    for house in house_info:
-        min_value = 99999
-        for chicken in chicken_info:
-            min_distance = abs(house[0] - chicken[0]) + abs(house[1] - chicken[1])
-            if min_distance < min_value:
-                min_value = min_distance
-        sum_list.append(min_value)
-
-    print(sum(sum_list))
-
-solution(n, m, chicken_map)
+temp = 1e9
+for candidate in candidates:
+    temp = min(temp, get_distance(candidate))
+print(temp)

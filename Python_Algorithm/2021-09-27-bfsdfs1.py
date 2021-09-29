e5eeab747dfe643c4ad660e57e854c62f0ec7801
SeokMin <dltjrals2@naver.com>
2021-09-29 10:32:12 +0900

    Python 문제 풀이 코드 : 2021-09-29 bfs/dfs 문제 풀이 추가
    1. 2021-09-29 bfs/dfs 문제 풀이 추가
    (특정 도시의 거리, 연구소)
    
    해결 :
    관련 :
    참고 :

---
+++ Python_Algorithm/2021-09-27-bfsdfs1.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+# n : 도시의 개수
+# m : 도로의 개수
+# k : 거리 정보
+# x : 출발 도시의 번호
+n, m, k, x = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+
+distance = [-1] * (n + 1)
+distance[x] = 0
+
+# BFS
+queue = deque([x])
+while queue:
+    now = queue.popleft()
+    for next_node in graph[now]:
+        if distance[next_node] == -1:
+            distance[next_node] = distance[now] + 1
+            queue.append(next_node)
+
+check = False
+for i in range(1, n + 1):
+    if distance[i] == k:
+        print(i)
+        check = True
+
+if check == False:
+    print(-1)
